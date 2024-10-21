d01ae4648476d30d2dd1f3ab6005dc4321a06289
Sachit Bhardwaj <103655068+sachit-bhardwaj@users.noreply.github.com>
2024-10-21 14:44:33 +0530

    Update zeroDivision.java

--- zeroDivision.java
+++ zeroDivision.java
@@ -1,7 +1,7 @@
 public class zeroDivision {
     public static void main(String[] args) {
         try {
-            int result = 10 / 0; // This will cause an ArithmeticException
+            int result = 10 / 0; 
             System.out.println("Result: " + result);
         } catch (ArithmeticException e) {
             System.out.println("Exception caught: Division by zero is not allowed.");
